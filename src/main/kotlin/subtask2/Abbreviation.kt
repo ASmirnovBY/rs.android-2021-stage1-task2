0cceb844e4924868332ee1d6e1327e7c2c4817b6
Alex Smirnou <alex.snirnou94@gmail.com>
2021-06-19 11:37:32 +0300

    Fix and end Abbreviation task

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -2,6 +2,11 @@ package subtask2
 
 class Abbreviation {
 
+    companion object {
+        private const val YES = "YES"
+        private const val NO = "NO"
+    }
+
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
         val aUpper = a.toUpperCase()
@@ -11,7 +16,7 @@ class Abbreviation {
     private fun search(aUpper: String, b: String): String {
         val aToCharArray = aUpper.toCharArray()
         val bToCharArray = b.toCharArray()
-        var startIndex = 0;
+        var startIndex = -1;
         for(i in b.indices) {
            startIndex = searchCharBinAIndex(aToCharArray, bToCharArray[i], startIndex)
            if (startIndex == -1) {
@@ -23,16 +28,12 @@ class Abbreviation {
 
     private fun searchCharBinAIndex(a: CharArray, charInB: Char,
                                     startIndex: Int): Int {
-       for (startIndex in a.indices) {
-           if (a[startIndex] == charInB) {
-               return startIndex
+       val shiftStartIndex = startIndex + 1
+       for (i in shiftStartIndex until a.size) {
+           if (a[i] == charInB) {
+               return i
            }
        }
         return -1
-    }
-
-    companion object {
-        private const val YES = "YES"
-        private const val NO = "NO"
     }
 }
