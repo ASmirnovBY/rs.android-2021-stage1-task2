28f21005fee1d48c5e92f0022554860178743d71
Alex Smirnou <alex.snirnou94@gmail.com>
2021-06-20 01:07:08 +0300

    Start DateFormatter task

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,27 @@
 package subtask1
 
 class DateFormatter {
+    companion object {
+        private val DAY_IN_MONTH = arrayOf(31, 28, 31, 30,
+                                           31, 30, 31, 31,
+                                           30, 31, 30, 31, 29)
+
+        private val DAY_OF_WEEK = arrayOf("Такого дня не существует", "понедельник", "вторник",
+        "среда", "четверг", "пятница", "суббота", "воскресенье")
+
+        private val MONTH_NAME = arrayOf("високосный февраль","января", "февраля", "марта",
+            "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря")
+    }
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+
+        return ""
+    }
+
+    private fun isLeapYear(year: String): Int {
+        val parsYear = Integer.parseInt(year)
+        return if ((parsYear % 4 === 0) && ((parsYear % 100 !== 0)
+                    || (parsYear % 400 === 0))) 1 else 0
     }
 }
